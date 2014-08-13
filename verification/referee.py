6bf36360a3e28ff87d3bf74c1fbc953b6e1ff799
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-08-13 17:55:57 +1200

    fix referee

--- verification/referee.py
+++ verification/referee.py
@@ -29,19 +29,8 @@ checkio.referee.cover_codes
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.code import CheckiORefereeCode
-from checkio.referees import cover_codes
-from checkio.referees import checkers
 
 from tests import TESTS
-
-
-from checkio import api
-
-from checkio.referees.io import CheckiOReferee
-
-REQ = 'req'
-REFEREE = 'referee'
-
 
 class CheckiORefereeCodeScore(CheckiORefereeCode):
 
